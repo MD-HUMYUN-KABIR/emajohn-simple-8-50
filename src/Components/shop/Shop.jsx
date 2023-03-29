ffc61d0f6b4546896e00bedb41057f10623fe80e
MD-HUMYUN-KABIR <misbahulhaque0840@gmail.com>
2023-03-29 20:24:49 +0600

    m

--- src/Components/shop/Shop.jsx
+++ src/Components/shop/Shop.jsx
@@ -6,7 +6,8 @@ import './Shop.css'
 const Shop = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([]);
-    // useState muloto customer er kria collect kore,,,akhane kuno kichu store kore rakha hoy then akhan theke property gula nie onno jaygay dekhanu hoy....jemon first e amra product gula fetch kore akhane store korechi but akhn customer cart e jaa jaa add korbe sei sobgula akhane akta array hisebe store korbo then onno segula k jaygay dekhabo
+    // useState muloto customer er kria collect kore,,,akhane kuno kichu store kore rakha hoy then akhan theke property gula nie onno jaygay dekhanu hoy....jemon first e amra product gula fetch kore akhane store korechi but akhn customer cart e jaa jaa add korbe sei sobgula akhane akta array hisebe store korbo then onno jaygay segula k dekhabo
+
     useEffect(() => {
         fetch('products.json')
         .then(res => res.json())
@@ -18,21 +19,27 @@ const Shop = () => {
     useEffect(()=> {
         const storedCart = getShoppingCart();
         const savedCart = [];
+        console.log('save cart line 21 shop.jsx', savedCart);
         //step-1 get id
         for(const id in storedCart){
             // step 2 get the product by using id
             const addedProduct = products.find(product => product.id === id);
+            // localstorage e set kora id er product er sathe "main products" er jei product ta milbe seta k "addedProduct" variable e set korbo. then localstorage e add kora id er quantity joto hobe seta abar addedProduct variable er prottekta product er quantity hisebe set kore dibo..jeno ai quantity ta local storage e set kora quantityr soman thake ar seta k e UI te dekhabo. tahole ei productgula k akta array te push kore sekhan theke UI te dekhabo 
+
+
             // console.log(addedProduct);
            
-            // step 3 add quantity
+            // step 3 add quantity first e localstorage e addedproduct na o thakte pare ,ar na thakle undefined dekhabe tai product thakle setar upore kaj korbo
             if(addedProduct){
                 const quantity = storedCart[id];
                 addedProduct.quantity = quantity;
+                // local storage e add kora idr quantity and product er idr quantity soman kore dilam
                 savedCart.push(addedProduct);
             }
             // console.log('added product', addedProduct);
         }
-        setCart(savedCart);
+         setCart(savedCart); // akhane hocche j automatic jeno ager cart er moto e akta array setCart e set hoy
+        //akhn finally cart e ai array ta e set hbe and ata k e UI te dekhabe.ager jei cart ta chilo sekhane prottekta product er quantity chilo na but akhn er cart e quantity localstorage er product er quantityr same kore dewa hoyeche
 
     }, [products])// ader useEffect kore set kora products gula akhane rehe dilam ...akhn ai product gula theke abar kaj korte parbo
 
@@ -40,7 +47,9 @@ const Shop = () => {
 // nicher folder theke uporer folder e kuno props value pathanu jay na..but upore theke kuno function likhe seta k props hisebe nicher kuno folder e pathie dile and sekhan theke abar ai funtion k  parameter soho call korle uporer function e  value automatic chole asbe
     const handleAddToCart = (product) => {
         const newCart = [...cart, product];
-        setCart(newCart);
+        //aikhane click er maddhome akta array setcart a cart hisebe set kortechi jekhane default product thakbe jei product e kuno quantityr value thakbe na but abar useEffect kore oi ak e array k setCart e cart hisebe set kortechi jei product er quantity hobe localStorage e add kora product er soman
+
+        setCart(newCart);//akhane hocche jeno click korle e setcart e cart array add hoy and useEffect theke o setCart e arekta same cart array e automatic Vabe set kora hoyeche
         addToDb(product.id);
     }
 
